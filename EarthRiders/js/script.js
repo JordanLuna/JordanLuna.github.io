1437b8f649373c983379c8efa11373e13bfe1d31
Lunachick <jordan.luna01@gmail.com>
2020-02-27 15:15:14 -0600

    deleted first back to top button

--- EarthRiders/js/script.js
+++ EarthRiders/js/script.js
@@ -91,18 +91,3 @@ $(".instagram").hover(function () {
 });
 
 
-//jQuery for back to top button courtsey https://codepen.io/desirecode/pen/MJPJqV
-
-$(document).ready(function(){ 
-    $(window).scroll(function(){ 
-        if ($(this).scrollTop() > 100) { 
-            $('#scroll').fadeIn(); 
-        } else { 
-            $('#scroll').fadeOut(); 
-        } 
-    }); 
-    $('#scroll').click(function(){ 
-        $("html, body").animate({ scrollTop: 0 }, 600); 
-        return false; 
-    }); 
-});

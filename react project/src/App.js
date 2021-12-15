0715c8886e2ff14ff785f1b07d7f7c372a528de1
JordanLuna <jordan.luna01@gmail.com>
2021-12-15 08:51:18 -0600

    building the project

--- react project/src/App.js
+++ react project/src/App.js
@@ -1,5 +1,15 @@
 function App() {
-  return <div>Hello!</div>;
+  return (
+  <div>
+    <h1>My To Dos</h1>
+    <div className='card'>
+      <h2>Title</h2>
+      <div>
+        <button className='btn'>delete</button>
+      </div>      
+    </div>
+  </div>
+  );
 }
 
 export default App;

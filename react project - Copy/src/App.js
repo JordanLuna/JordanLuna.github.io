fca4aecee6dde268db6a731e298075a7cd585d24
JordanLuna <jordan.luna01@gmail.com>
2022-01-03 14:34:40 -0600

    editing

--- react project - Copy/src/App.js
+++ react project - Copy/src/App.js
@@ -1,16 +1,5 @@
-
-import Todo from './components/Todo';
-
 function App() {
-  return (
-  <div>
-    <h1>My To-Dos</h1>
-    <Todo text='Learn React'/>
-    <Todo text='Master React'/>
-    <Todo text='Pwn React'/>
-    
-  </div>
-  );
+  return <div> </div>
 }
 
 export default App;

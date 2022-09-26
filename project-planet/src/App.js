bfa6c256bf014ebbf719a168c384acf70e791b1c
JordanLuna <jordan.luna01@gmail.com>
2022-09-26 10:08:06 -0500

    no

--- project-planet/src/App.js
+++ project-planet/src/App.js
@@ -1,32 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-import User from './User';
- 
+import React from 'react';
+import TodoList from './TodoList'
 
 function App() {
   return (
-    <div className="App">
-      <User userName='First User' tweet="What a nice day"/>
-      <User userName='Tony Stark' tweet="I am Iron Man"/>
-      <User userName='Bobb the Bubble' tweet="Halo a chairdean!"/>
-      <User userName='Iain agus Anndra' tweet="Obh Obh!!"/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-         
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <TodoList />
+  )
 }
 
 export default App;
